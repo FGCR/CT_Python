cf8857138e70800b1312900017bafe5a82dfabfe
FGCR <astromodeus@protonmail.com>
2021-08-04 19:22:48 +0900

    Commit Test(Repository name changed)

--- Solutions/1_TwoSum.py
+++ Solutions/1_TwoSum.py
@@ -2,6 +2,15 @@ def Two_Sum(vals, target):
     valdict = dict()
 
     for index, val in enumerate(vals):
-        #if Duplicate,
+        #if Duplicate, Overwrite
+        #if answer contains 2 same vals (ex: 3 + 3 = 6), just filtered cause finding target starts at begin.
         valdict[val] = index
 
+    for i in range(len(vals)):
+        other_one = target - vals[i]
+        if other_one in valdict:
+            other_one_idx = valdict[other_one]
+            if other_one_idx != i:
+                return [i, valdict[other_one]]
+
+    return None
